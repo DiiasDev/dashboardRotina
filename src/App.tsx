6f7ac0b717881042feb9bd22d803a18aeda88c25
DiiasDev <devpdias@gmail.com>
2025-05-30 11:31:37 -0300

    Remoção de css globals

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./styles/App.css";
 import HomePage from "./Pages/HomePage/homePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/theme.css";
-import "./styles/globals.css";
 import LoginPage from "./Pages/LoginPage/loginPage";
 function App() {
   return (
