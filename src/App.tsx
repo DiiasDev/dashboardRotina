d3d90cfbee94642631e2af8de4383a71c56cb584
DiiasDev <devpdias@gmail.com>
2025-06-01 23:22:36 -0300

    Finalização do navigation (adição de estilos e lógica para mudar os temas das páginas).

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import "./styles/App.css";
 import HomePage from "./Pages/HomePage/homePage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import "./styles/theme.css";
 import "./styles/global-theme.css";
 import LoginPage from "./Pages/LoginPage/loginPage";
 import Navigation from "./Components/Navigation/navigation";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
+function AppContent() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/";
+
+  return (
+    <div className="App">
+      {!isLoginPage && <Navigation />}
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/homepage" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
+}
+
 function App() {
   return (
     <ThemeProvider>
-      <div className="App">
-        <Navigation />
-        <Router>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/homepage" element={<HomePage />} />
-          </Routes>
-        </Router>
-      </div>
+      <Router>
+        <AppContent />
+      </Router>
     </ThemeProvider>
   );
 }
--- src/Pages/LoginPage/loginPage.tsx
+++ src/Pages/LoginPage/loginPage.tsx
@@ -1,16 +1,15 @@
-import Footer from "../../Components/Footer/footer";
+import React from "react";
 import FormLogin from "../../Components/FormLogin/formLogin";
+import Footer from "../../Components/Footer/footer";
 import styles from "./styles.module.css";
 
 export default function LoginPage() {
   return (
-    <>
-      <main className={styles.container}>
-        <div className={styles.loginWrapper}>
-          <FormLogin />
-        </div>
-      </main>
+    <div className={styles.loginPageContainer}>
+      <div className={styles.loginContent}>
+        <FormLogin />
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
