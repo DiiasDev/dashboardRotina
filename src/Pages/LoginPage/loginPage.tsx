c0aa2e822dd4a919d4ba497e524dcdb5184a1bef
DiiasDev <devpdias@gmail.com>
2025-06-24 16:09:36 -0300

    feat: adicionando estilos e logo para tela de login.

--- src/Pages/LoginPage/loginPage.tsx
+++ src/Pages/LoginPage/loginPage.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import FormLogin from "../../Components/FormLogin/formLogin";
 import Footer from "../../Components/Footer/footer";
+import ThemeToggle from "../../Components/ThemeToggle/ThemeToggle";
 import styles from "./styles.module.css";
+import logo from "../../Assets/images/logo.png"
 
 export default function LoginPage() {
   return (
     <div className={styles.loginPageContainer}>
+      <ThemeToggle />
       <div className={styles.loginContent}>
+        <div className={styles.logoContainer}>
+          <img 
+            src={logo} 
+            alt="DSYS Logo" 
+            className={styles.logo}
+          />
+          <h1 className={styles.brandName}>DSYS</h1>
+        </div>
         <FormLogin />
       </div>
       <Footer />
